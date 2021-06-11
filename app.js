8ca4ebe5840347c5132c97e6f7ee980d3ddc12f8
Dalen Chan <dalen.chan@hotmail.com>
2021-06-11 13:53:36 +0100

    Communicating with API through fetch()

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const form = document.getElementById('form').addEventListener('submit', myFunc)
+// Variables 
+const form = document.getElementById('form');
+const userInput = document.querySelector('.user-input');
 
-function myFunc(e){
-    console.log('Success');
+// Event Listeners 
+form.addEventListener('submit', fetchData);
+
+// API Key
+apik = 'eab55233968fcfc4a028e68bbb6f02b2'
+
+// API fetch 
+function fetchData (e){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput.value + '&appid=' + apik)
+    .then(res => res.json())
+    .then(data => console.log(data))
     e.preventDefault();
 }
